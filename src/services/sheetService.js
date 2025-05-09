82d596d95f32367b21a4031778f1cdea46ed87cb
Hailey Cooperrider <haileycooperrider@gmail.com>
2025-05-09 17:14:22 +1000

    extract sheetid to env file

--- src/services/sheetService.js
+++ src/services/sheetService.js
@@ -2,10 +2,10 @@
 import axios from 'axios'
 
 // Use the published-to-web ID (the part after /d/e/ in your URL)
-const PUBLISH_ID = '2PACX-1vSn-XvdXYEy0CVgHIkXIoQd4Pb2YfFQGLy5_4kOeu2DFcaOICW0CEQdw5QRqeusTvkJico5jFhYeo9C'
+const SHEET_ID = import.meta.env.VITE_SHEET_ID
 
 // Build the CSV URL from that
-const CSV_URL = `https://docs.google.com/spreadsheets/d/e/${PUBLISH_ID}/pub?gid=0&single=true&output=csv`
+const CSV_URL = `https://docs.google.com/spreadsheets/d/e/${SHEET_ID}/pub?gid=0&single=true&output=csv`
 
 export async function fetchHexData() {
   const resp = await axios.get(CSV_URL)
@@ -19,4 +19,6 @@ export async function fetchHexData() {
     }, {})
   })
 }
+
+console.log('SHEET_ID:', SHEET_ID)
 
